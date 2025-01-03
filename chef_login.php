9ac6c48e935d9b10b005b550342ea692bebf7aef
Ronak-ved <168576820+Ronak-ved@users.noreply.github.com>
2025-01-03 20:08:54 +0530

    email added instead of mobile in login page

--- chef_login.php
+++ chef_login.php
@@ -11,11 +11,11 @@ if ($conn->connect_error) {
 if (isset($_POST['sbmt'])) { // Check if the form has been submitted
     // Collect data from the form
     
-    $mobile = $_POST['mobile'];
+    $email = $_POST['email'];
     $password = $_POST['password']; // Securely hash the password
 
     // fetch data from the database
-    $sql = "SELECT * FROM chefs WHERE chef_mobile = '$mobile' and chef_password = '$password' " or die('query failed');
+    $sql = "SELECT * FROM chefs WHERE chef_email = '$email' and chef_password = '$password' " or die('query failed');
     $stmt = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($stmt)){
@@ -47,10 +47,10 @@ $conn->close(); // Close the database connection
             <div class="chef-loginform">
                 <h1 id="loginheader">Login</h1>
                 <div class="inputbox">
-                    <input type="text" name="mobile" placeholder="Mobile Number" required>
+                    <input type="text" name="email" placeholder="Enter Email" required>
                 </div>
                 <div class="inputbox">
-                    <input type="password" name="password" placeholder="Password" required>
+                    <input type="password" name="password" placeholder="Enter Password" required>
                 </div>
                 <div class="register-loginlink">
                    <p> Don't have a account?<a href="chef_registration.php">Register</a>
--- login.php
+++ login.php
@@ -11,11 +11,11 @@ if ($conn->connect_error) {
 if (isset($_POST['sbmt'])) { // Check if the form has been submitted
     // Collect data from the form
     
-    $mobile = $_POST['mobile'];
+    $email = $_POST['email'];
     $password = $_POST['password']; // Securely hash the password
 
     // fetch data from the database
-    $sql = "SELECT * FROM users WHERE mobile = '$mobile' and password = '$password' " or die('query failed');
+    $sql = "SELECT * FROM users WHERE email = '$email' and password = '$password' " or die('query failed');
     $stmt = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($stmt)){
@@ -47,10 +47,10 @@ $conn->close(); // Close the database connection
             <div class="loginform">
                 <h1 id="loginheader">Login</h1>
                 <div class="inputbox">
-                    <input type="text" name="mobile" placeholder="Mobile Number" required>
+                    <input type="text" name="email" placeholder="Enter Email" required>
                 </div>
                 <div class="inputbox">
-                    <input type="password" name="password" placeholder="Password" required>
+                    <input type="password" name="password" placeholder="Enter Password" required>
                 </div>
                 <div class="register-loginlink">
                    <p> Don't have a account?<a href="registration.php">Register</a>
